66651babbecb4713717dfabf578ddaa30aa220d2
Chirag Bhandakkar <chirag.b@framedx.com>
2025-05-20 12:08:22 +0530

    phone webpage

---
+++ frontend/src/auth/LoginWrapper.tsx
@@ -0,0 +1,20 @@
+const LoginWrapper = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      {/* 🔪 Shared Background Image Section */}
+      <div
+        className="relative h-[50vh] w-full bg-cover bg-center"
+        style={{ backgroundImage: "url('/dinner.jpg')" }}
+      >
+        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-transparent to-white" />
+      </div>
+
+      {/* 📱 Page-specific content */}
+      <div className="flex-1 px-6 pt-10 pb-10 text-center bg-white">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default LoginWrapper;
